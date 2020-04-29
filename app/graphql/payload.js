db30554c8d57d4dde69961f8e3cf024e56f6fd71
Akash raj(CMD) <akashraj9828@gmail.com>
2020-04-29 13:26:59 +0530

    showcase itme limit 6

--- app/graphql/payload.js
+++ app/graphql/payload.js
@@ -37,7 +37,7 @@ const userPayload = username => ({
             totalCount
           }
           itemShowcase {
-            items(first: 10) {
+            items(first: 6) {
               nodes {
                 ... on Repository {
                   isFork
